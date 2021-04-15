95217c4f9390856ff6b80a768b01036ff7160cf7
Lianjieshan <51137255+Lianjieshan@users.noreply.github.com>
2021-04-14 22:26:12 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exercise1
+{
+    public class Student 
+    {
+        string Name, Address;
+        int Mobile;
+        public void GetData()
+            {
+                Console.WriteLine("Enter your Name");
+                Name = Console.ReadLine();
+                Console.WriteLine("Enter your Address");
+                Address = Console.ReadLine();
+                Console.WriteLine("Enter your Mobile Number:");
+                Mobile = Convert.ToInt32(Console.ReadLine()) ;
+            }
+        
+        public void DisplayData()
+            {
+            Console.WriteLine("Student Name:" + Name);
+            Console.WriteLine("Student Address:" + Address);
+            Console.WriteLine("Student Mobile:" + Mobile);
+            }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            s1.GetData();
+            s1.DisplayData();
+            Console.ReadKey();
+            
+        }
+    }
+}
